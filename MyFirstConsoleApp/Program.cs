933ded8a4e16a677b27804c4308a498dae41cb67
Maja Daniela Hansen <mdha54168.ucl.dk>
2023-06-17 16:55:02 +0200

    Made a OperatorExamples method

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace MyFirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OperatorExamples();
+        }
+
+        private static void OperatorExamples()
+        {
+            int width = 3;
+            width++;
+
+            int height = 2 + 4;
+            int area = width * height;
+
+            Console.WriteLine(area);
+
+            while (area > 50)
+            {
+                height++;
+                area = width * height;
+            }
+
+            do 
+            { 
+                width--;
+                area = width * height;
+            } while (area > 25);
+
+            string result = "the area";
+            result = result + " is " + area;
+            Console.WriteLine(result);
+
+            bool truthValue = true;
+            Console.WriteLine(truthValue);
+        }
+    }
+}
