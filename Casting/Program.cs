419b921c2c8c4674c803ea06fc0eda251752ad26
Maja Daniela Hansen <mdha54168.ucl.dk>
2023-07-03 10:22:30 +0200

    casting

--- Casting/Program.cs
+++ Casting/Program.cs
@@ -8,6 +8,14 @@
             int myIntValue = (int)myFloatValue;
 
             Console.WriteLine("myIntValue is " + myIntValue);
+
+
+            //Automatically converts to float
+            int myInt = 36;
+            float myFloat = 16.4F;
+            myFloat = myInt + myFloat;
+
+            Console.WriteLine("myFloat is " + myFloat);
         }
     }
 }
